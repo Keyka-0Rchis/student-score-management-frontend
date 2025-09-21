a52ea70ffa399b8ff695c7c6de341acb0a7d104b
Keyka-0Rchis <tkwsm1221@gmail.com>
2025-09-21 15:00:40 +0900

    Headerの作成

---
+++ student-score-management-frontend/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom';
+//module.cssをインポート。stylesは慣習的な名前。
+
+function Header(props: { title: string ,onMenuClick : () => void }) {
+  return (
+    <div className={styles.header}>
+      <i className={`fas fa-bars ${styles.bars}`} onClick={props.onMenuClick}></i>
+      <Link to="/mainmenu" className={styles.titleLink}>
+        <h1 className={styles.title}>{props.title}</h1>
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
