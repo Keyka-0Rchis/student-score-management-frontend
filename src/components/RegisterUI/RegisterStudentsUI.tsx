455f1bcb109d72fb610ce5f20d4ed55044a74f97
Keyka-0Rchis <tkwsm1221@gmail.com>
2025-09-23 00:50:41 +0900

    APIの呼び出しURLの誤りを訂正

--- src/components/RegisterUI/RegisterStudentsUI.tsx
+++ src/components/RegisterUI/RegisterStudentsUI.tsx
@@ -35,7 +35,7 @@ function RegisterStudentsUI(){
         });
 
         try {
-            const response = await fetch("http://localhost:8080/api/students", {
+            const response = await fetch("http://localhost:8080/api/students/registerStudents", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
